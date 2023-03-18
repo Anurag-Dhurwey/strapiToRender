a29856b2fbac1c128c218b49d6e252a63f89b3aa
Anurag dhurwey <anuragdhurwey9211@gmail.co>
2023-03-18 13:15:50 +0530

    email confirmation is initialized

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,22 @@
 module.exports = ({ env }) => ({
-    // ...
-    email: {
-      config: {
-        provider: 'sendgrid',
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
+  // ...
+  email: {
+    config: {
+      provider:env('EMAIL_PROVIDER', 'nodemailer'),
+      providerOptions: {
+        host: env('EMAIL_SMTP_HOST', 'smtp-relay.sendinblue.com'),
+        port: env('EMAIL_SMTP_PORT', 587),
+        auth: {
+          user: env('EMAIL_SMTP_USER'),
+          pass: env('EMAIL_SMTP_PASS'),
         },
-        settings: {
-          defaultFrom: 'anuragdhurwey9211@gmail.com',
-          defaultReplyTo: 'sevkalidhurwey236@gmail.com',
-          testAddress: 'juliasedefdjian@strapi.io',
-        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env('EMAIL_ADDRESS_FROM','anuragdhurwey9211@gmail.com'),
+        defaultReplyTo:env('EMAIL_ADDRESS_REPLY','anuragdhurwey9211@gmail.com'),
       },
     },
-    // ...
-  });
+  },
+  // ...
+});
